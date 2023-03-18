38ba6625d45992f6cc0d5e41341bed9975503eda
wook@desktop05 <rudollee@gmail.com>
2023-03-18 12:25:01 +0900

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string path = Directory.GetCurrentDirectory();
+        string oldName = "";
+        string newName = "";
+
+        if (args.Length == 3)
+        {
+            path = args[0];
+            oldName = args[1];
+            newName = args[2];
+        }
+        else if (args.Length == 2)
+        {
+            oldName = args[0];
+            newName = args[1];
+        }
+        else
+        {
+            Console.WriteLine("Usage: batchRename.exe [path] oldpart newpart");
+            return;
+        }
+
+        DirectoryInfo directory = new DirectoryInfo(path);
+        FileInfo[] files = directory.GetFiles();
+
+        foreach (FileInfo file in files)
+        {
+            if (file.Name.Contains(oldName))
+            {
+                string newFileName = file.Name.Replace(oldName, newName);
+                file.MoveTo(Path.Combine(file.DirectoryName, newFileName));
+            }
+        }
+    }
+}
